c4282511e768f8c214d5ce98667f1f72536a5205
Jess247 <61388219+Jess247@users.noreply.github.com>
2021-07-26 18:18:49 +0200

    add and get contacts

--- CRM/src/models/crmModel.js
+++ CRM/src/models/crmModel.js
@@ -28,3 +28,4 @@ export const ContactSchema = new Schema({
         default: Date.now
     }
 });
+    
--- CRM/src/routes/crmRoutes.js
+++ CRM/src/routes/crmRoutes.js
@@ -1,3 +1,5 @@
+import {addNewContact,
+        getContacts} from '../controllers/crmController'
 const routes = (app) => {
     app.route('/contact')
         
@@ -6,13 +8,10 @@ const routes = (app) => {
             console.log(`Request from: ${req.originalUrl}`)
             console.log(`Request type: ${req.method}`)
             next();
-        }, (req, res, next) => {
-            res.send('GET request successful!')
-        })
+        }, getContacts)
         
 
-        .post((req,res) =>
-        res.send('POST request successful!'));
+        .post(addNewContact);
 
     app.route('/contact/:contactID')
         .put((req,res) =>

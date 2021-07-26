2c292a6f8b88bd0d6a20fb7b83408d53dfc8ccb1
Jess247 <61388219+Jess247@users.noreply.github.com>
2021-07-26 12:10:07 +0200

    update

--- CRM/index.js
+++ CRM/index.js
@@ -1,1 +1,15 @@
 import express from 'express';
+
+// execution of express
+const app = express();
+// port number
+const PORT = 4000; 
+
+app.get('/', (req, res) =>
+    res.send(`Node and express server running on port ${PORT}`)
+);
+
+// to console
+app.listen(PORT, () => 
+    console.log(`Your server is running on port ${PORT}`)
+);
